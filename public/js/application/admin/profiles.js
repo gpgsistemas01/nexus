231b92e2c26cd1afde056651518fe510f6a2ad05
gpgsistemas01 <gpgsistemas01@gmail.com>
2026-06-09 13:45:26 -0600

    Use profile success codes only

--- public/js/application/admin/profiles.js
+++ public/js/application/admin/profiles.js
@@ -1,6 +1,16 @@
 import { getSuccessMessage } from "../../constants/apiMessages.js";
 import { getAllProfilesRequest, registerProfileRequest, updateProfileRequest } from "../../services/admin/profileService.js";
 
+const getProfileSuccessResponse = ({ data }) => {
+
+    const { code, profile } = data;
+
+    return {
+        message: getSuccessMessage(code),
+        data: profile
+    };
+};
+
 export const getAllProfiles = async (params = {}) => {
 
     const response = await getAllProfilesRequest({ params });
@@ -13,13 +23,8 @@ export const registerProfile = async ({ formData }) => {
     const response = await registerProfileRequest({ data: formData });
 
     const { data } = response;
-    const { code, profile } = data;
-    let message = getSuccessMessage(code);
 
-    return {
-        message,
-        data: profile
-    };
+    return getProfileSuccessResponse({ data });
 }
 
 export const updateProfile = async ({ formData, id }) => {
@@ -27,11 +32,6 @@ export const updateProfile = async ({ formData, id }) => {
     const response = await updateProfileRequest({ data: formData, id });
 
     const { data } = response;
-    const { code, profile } = data;
-    let message = getSuccessMessage(code);
 
-    return {
-        message,
-        data: profile
-    };
+    return getProfileSuccessResponse({ data });
 }
