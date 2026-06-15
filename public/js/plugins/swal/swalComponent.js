495464b4d54ff15b1708ef72cff189af01367a12
gpgsistemas01 <gpgsistemas01@gmail.com>
2026-06-15 13:28:05 -0600

    Simplificar alerta de stock en salidas

--- public/js/plugins/swal/swalComponent.js
+++ public/js/plugins/swal/swalComponent.js
@@ -1,8 +1,9 @@
-import { showToast } from "./baseSwal.js";
+import { showModal, showToast } from "./baseSwal.js";
 
 export const notifications = {
     showSuccess: (title) => showToast({ title, icon: 'success' }),
     showWarning: (title) => showToast({ title, icon: 'warning' }),
     showError: (title) => showToast({ title, icon: 'error' }),
-    showInfo: (title) => showToast({ title })
+    showInfo: (title) => showToast({ title }),
+    showModal: ({ title, text, icon = 'info' }) => showModal({ title, text, icon })
 }
