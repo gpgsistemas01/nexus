82fdd64612296f71f2553f3a6266591bfe895d2b
gpgsistemas01 <gpgsistemas01@gmail.com>
2026-06-16 10:23:08 -0600

    Restore login form normalizeData

--- public/js/pages/home/loginPage.js
+++ public/js/pages/home/loginPage.js
@@ -5,7 +5,56 @@ import { loginValidators } from "../../utils/validations/validators.js";
 import { FORM_SELECTORS } from "../../constants/selectors.js";
 
 const formId = FORM_SELECTORS.LOGIN;
+const rememberCredentialsKey = 'loginRememberedCredentials';
+const loginForm = document.querySelector(formId);
+const nameInput = loginForm?.querySelector('[name="name"]');
+const passwordInput = loginForm?.querySelector('[name="password"]');
+const rememberCredentialsInput = loginForm?.querySelector('[name="rememberCredentials"]');
+
+const getRememberedCredentials = () => {
+
+    const credentials = localStorage.getItem(rememberCredentialsKey);
+
+    if (!credentials) return null;
+
+    try {
+
+        return JSON.parse(credentials);
+
+    } catch {
+
+        localStorage.removeItem(rememberCredentialsKey);
+        return null;
+    }
+};
+
+const loadRememberedCredentials = () => {
+
+    const credentials = getRememberedCredentials();
+
+    if (!credentials) return;
+
+    if (nameInput) nameInput.value = credentials.name || '';
+    if (passwordInput) passwordInput.value = credentials.password || '';
+    if (rememberCredentialsInput) rememberCredentialsInput.checked = true;
+};
+
+const updateRememberedCredentials = (formData) => {
+
+    if (formData.rememberCredentials !== 'true') {
+
+        localStorage.removeItem(rememberCredentialsKey);
+        return;
+    }
+
+    localStorage.setItem(rememberCredentialsKey, JSON.stringify({
+        name: formData.name,
+        password: formData.password
+    }));
+};
+
 document.getElementById('submitBtn').textContent = 'Ingresar';
+loadRememberedCredentials();
 
 useForm({
     selector: formId,
@@ -22,6 +71,7 @@ useForm({
 
         const response = await login({ formData });
 
+        updateRememberedCredentials(formData);
         localStorage.setItem('showSuccessToast', response.message);
         window.location.replace('/productos');
     }
