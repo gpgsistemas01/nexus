88c02329f684043dac845e0df0c3a44d2f47b2da
gpgsistemas01 <gpgsistemas01@gmail.com>
2026-07-01 17:50:11 -0600

    feat: Agregar puerto explicito

--- src/app.js
+++ src/app.js
@@ -162,7 +162,9 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT);
+server.listen(PORT, "0.0.0.0", () => {
+    console.log(`Servidor escuchando en puerto ${PORT}`);
+});
 
 process.on('warning', (warning) => {
     logger.warn({ warning }, 'Warning detectado');
