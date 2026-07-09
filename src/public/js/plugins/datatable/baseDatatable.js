5a811e884b0782f95ad060dc54b45e94a868b909
gpgsistemas01 <gpgsistemas01@gmail.com>
2026-07-09 16:02:22 -0600

    Remove MDB initialization from table inputs

--- src/public/js/plugins/datatable/baseDatatable.js
+++ src/public/js/plugins/datatable/baseDatatable.js
@@ -40,17 +40,6 @@ const adjustDataTableColumns = (table) => {
     if (typeof table?.responsive?.recalc === 'function') table.responsive.recalc();
 };
 
-const initDataTableMdbInputs = (tableNode) => {
-
-    if (!tableNode || !globalThis.mdb?.Input) return;
-
-    tableNode.querySelectorAll('[data-mdb-input-init]').forEach((wrapper) => {
-        const instance = globalThis.mdb.Input.getOrCreateInstance(wrapper);
-
-        instance.update();
-    });
-};
-
 const initDataTableMdbComponents = (table) => {
 
     const tableNode = table?.table?.().node?.();
@@ -58,7 +47,6 @@ const initDataTableMdbComponents = (table) => {
     if (!tableNode) return;
 
     initMdbTooltips(tableNode);
-    initDataTableMdbInputs(tableNode);
 };
 
 export const createDataTable = ({ selector = DATATABLE_SELECTORS.MAIN, options = {} }) => {
