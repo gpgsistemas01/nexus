307133e6e44f13baf35bcfdc9c29df24e2af3016
gpgsistemas01 <gpgsistemas01@gmail.com>
2026-07-20 17:06:29 -0600

    Summarize selected product data in waste form

--- src/public/js/plugins/select2/domains/supplierProduct.js
+++ src/public/js/plugins/select2/domains/supplierProduct.js
@@ -1,23 +1,52 @@
 import { getSupplierProductOptions } from "../../../application/warehouse/products.js";
-import { initbaseSelect2, setMdbWrapperInputValue, toggleSelectOption } from "../baseSelect.js";
+import { initbaseSelect2, toggleSelectOption } from "../baseSelect.js";
 import { FORM_SELECTORS } from "../../../constants/selectors.js";
 
-const presentationDisplaySelector = FORM_SELECTORS.PRESENTATION_DISPLAY;
-const unitMeasureDisplaySelector = FORM_SELECTORS.UNIT_MEASURE_DISPLAY;
+const supplierSummarySelector = FORM_SELECTORS.SELECTED_PRODUCT_SUPPLIER;
+const presentationSummarySelector = FORM_SELECTORS.SELECTED_PRODUCT_PRESENTATION;
+const unitMeasureSummarySelector = FORM_SELECTORS.SELECTED_PRODUCT_UNIT_MEASURE;
+const baseSummarySelector = FORM_SELECTORS.SELECTED_PRODUCT_BASE;
+const heightSummarySelector = FORM_SELECTORS.SELECTED_PRODUCT_HEIGHT;
+
+const setSummaryText = ({ selector, value }) => {
+    const element = document.querySelector(selector);
+
+    if (!element) return;
+
+    const displayValue = value || '-';
+
+    element.textContent = displayValue;
+    element.dataset.value = displayValue;
+};
 
 export const setSupplierProductDisplayValues = ({
     modalSelector,
     data = {}
 }) => {
 
-    setMdbWrapperInputValue({
-        selector: `${ modalSelector } ${ presentationDisplaySelector }`,
-        value: data.presentationName || ''
+    setSummaryText({
+        selector: `${ modalSelector } ${ supplierSummarySelector }`,
+        value: data.supplierName
     });
 
-    setMdbWrapperInputValue({
-        selector: `${ modalSelector } ${ unitMeasureDisplaySelector }`,
-        value: data.unitMeasureName || ''
+    setSummaryText({
+        selector: `${ modalSelector } ${ presentationSummarySelector }`,
+        value: data.presentationName
+    });
+
+    setSummaryText({
+        selector: `${ modalSelector } ${ unitMeasureSummarySelector }`,
+        value: data.unitMeasureName
+    });
+
+    setSummaryText({
+        selector: `${ modalSelector } ${ baseSummarySelector }`,
+        value: data.productBase
+    });
+
+    setSummaryText({
+        selector: `${ modalSelector } ${ heightSummarySelector }`,
+        value: data.productHeight
     });
 };
 
@@ -26,13 +55,26 @@ const attachSupplierProductDisplayHandler = ({
     baseSelector
 }) => {
 
-    $(baseSelector).off('select2:select.supplierProductDisplay').on('select2:select.supplierProductDisplay', ({ params }) => {
+    $(baseSelector)
+        .off('.supplierProductDisplay')
+        .on('select2:select.supplierProductDisplay', ({ params }) => {
 
-        setSupplierProductDisplayValues({
-            modalSelector,
-            data: params?.data
+            setSupplierProductDisplayValues({
+                modalSelector,
+                data: params?.data
+            });
+        })
+        .on('select2:clear.supplierProductDisplay change.supplierProductDisplay', () => {
+
+            const selectedValue = $(baseSelector).val();
+
+            if (selectedValue) return;
+
+            setSupplierProductDisplayValues({
+                modalSelector,
+                data: null
+            });
         });
-    });
 };
 
 const initSupplierProductSelect = ({
