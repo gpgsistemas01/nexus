2a8369fd793f24dde221232cf97da89a01e0ccb0
gpgsistemas01 <gpgsistemas01@gmail.com>
2026-07-22 13:12:45 -0600

    Guard warehouse inventory header rendering

--- src/public/js/plugins/datatable/utils/warehouseInventoryDatatable.js
+++ src/public/js/plugins/datatable/utils/warehouseInventoryDatatable.js
@@ -3,6 +3,8 @@ import { formatCurrency, formatDecimal } from "../../../utils/formatUtils.js";
 
 export const renderWarehouseInventoryHeader = ({ tableElement, canSeeCost, canManageItems, stockTitle, costTitle }) => {
 
+    if (!tableElement) return;
+
     tableElement.innerHTML = `
         <thead>
             <tr>
