d562f374c2ceb806f8f0ebd6c5e8b312eb391c77
gpgsistemas01 <gpgsistemas01@gmail.com>
2026-07-23 10:08:14 -0600

    Filtra devoluciones como movimiento independiente

--- src/public/js/plugins/select2/domains/movementType.js
+++ src/public/js/plugins/select2/domains/movementType.js
@@ -14,6 +14,10 @@ export const getMovementTypeData = () => ([
     {
         id: 'ADJUSTMENT',
         text: 'Ajuste'
+    },
+    {
+        id: 'RETURN',
+        text: 'Devolución'
     }
 ]);
 
