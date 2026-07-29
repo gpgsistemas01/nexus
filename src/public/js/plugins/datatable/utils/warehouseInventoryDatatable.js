82611489bd5258c7dd5a3a04ab2b70bb0fb32e6d
gpgsistemas01 <gpgsistemas01@gmail.com>
2026-07-29 14:07:21 -0600

    Estabilizar mock de productos proveedor

--- src/public/js/plugins/datatable/utils/warehouseInventoryDatatable.js
+++ src/public/js/plugins/datatable/utils/warehouseInventoryDatatable.js
@@ -1,4 +1,4 @@
-import { renderMaterialName } from "./renderProductDatatable.js";
+import { renderMaterialName } from "./detailDatatableUtils.js";
 import { formatCurrency, formatDecimal } from "../../../utils/formatUtils.js";
 
 export const renderWarehouseInventoryHeader = ({ tableElement, canSeeCost, canManageItems, stockTitle, costTitle }) => {
