f857fb56b7609e1db2166b8020624569c72749f1
gpgsistemas01 <gpgsistemas01@gmail.com>
2026-08-10 15:34:23 -0600

    test: remove data table controller factory coverage

--- src/controllers/api/admin/roleController.js
+++ src/controllers/api/admin/roleController.js
@@ -1,16 +1,7 @@
 import { findAllRoles } from "../../../services/admin/roleService.js";
-import { getDataTableOrder, getDataTablePaging, getDataTableSearch } from "../../../utils/requestQueryUtils.js";
+import { createDataTableListController } from "../createDataTableListController.js";
 
-export const getAllRoles = async (req, res) => {
-    const { skip, take } = getDataTablePaging(req.query);
-    const search = getDataTableSearch(req.query);
-
-    const columns = ['name'];
-    const { orderBy, orderDir } = getDataTableOrder({
-        query: req.query,
-        columns
-    });
-
-    const result = await findAllRoles({ skip, take, search, orderBy, orderDir });
-    res.status(200).json(result);
-};
+export const getAllRoles = createDataTableListController({
+    findAll: findAllRoles,
+    columns: ['name']
+});
--- src/controllers/api/warehouse/presentationController.js
+++ src/controllers/api/warehouse/presentationController.js
@@ -1,24 +1,7 @@
 import { findAllPresentations } from "../../../services/warehouse/presentationService.js";
-import { getDataTableOrder, getDataTablePaging, getDataTableSearch } from "../../../utils/requestQueryUtils.js";
+import { createDataTableListController } from "../createDataTableListController.js";
 
-export const getAllPresentations = async (req, res) => {
-
-    const { skip, take } = getDataTablePaging(req.query);
-    const search = getDataTableSearch(req.query);
-
-    const columns = ['name'];
-    const { orderBy, orderDir } = getDataTableOrder({
-        query: req.query,
-        columns
-    });
-
-    const result = await findAllPresentations({
-        skip,
-        take,
-        search,
-        orderBy,
-        orderDir
-    });
-
-    return res.status(200).json(result);
-}
+export const getAllPresentations = createDataTableListController({
+    findAll: findAllPresentations,
+    columns: ['name']
+});
